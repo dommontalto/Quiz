317d41896a6f9a4606d40aa2e8c0a3656c366dc8
Dom Montalto <dommontalto@icloud.com>
2025-05-03 13:13:02 +0700

    Promp

--- Quiz/QuizApp.swift
+++ Quiz/QuizApp.swift
@@ -15,3 +15,13 @@ struct QuizApp: App {
         }
     }
 }
+
+/*
+Prompt:
+
+Build a quiz application.
+ - Users should be presented with a question that has 4 potential answers
+ - User should be able to select an answer and navigate to the next question until completion
+ - Show the question number the user is currently on
+ - Show a completion view with the option to view results or start quiz over.
+*/
