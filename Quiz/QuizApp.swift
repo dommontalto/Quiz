248cb9649959249dd2ac19917af8e82f227a27ab
Dom Montalto <dommontalto@icloud.com>
2025-05-03 13:07:33 +0700

    Initial commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Dominic Montalto on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
